9fe7021f3f35a6b934d6d1d807bd5db1b030f051
jennyschilling <112127053+jennyschilling@users.noreply.github.com>
2024-04-02 22:00:56 -0400

    Added file with SQL queries
     ...to demonstrate proper functionality

---
+++ SQL_queries.sql
@@ -0,0 +1,9 @@
+SELECT
+    c.is_vip,
+    p.product_name,
+    SUM(sales_fact.quantity_sold) AS total_quantity_sold
+FROM sales_fact
+JOIN customer_dim AS c ON sales_fact.customer_id = customer.customer_id
+JOIN product_dim AS p ON sales_fact.product_id = p.product_id
+GROUP BY c.is_vip, p.product_name
+ORDER BY total_quantity_sold DESC; 
