c569ea38b2413e22d7d8253021cf052d093e95e1
jennyschilling <112127053+jennyschilling@users.noreply.github.com>
2024-04-04 10:43:08 -0400

    Merged files create_tables and insert_data

--- 1_create_tables.sql
+++ 1_create_tables.sql
@@ -3,6 +3,11 @@ CREATE DATABASE `retail_sales_dw` /*!40100 DEFAULT CHARACTER SET latin1 */ /*!80
 
 USE retail_sales_dw;
 
+
+###############
+# create tables
+###############
+  
 # DROP TABLE `sales_fact`;
 CREATE TABLE sales_fact (
 sale_id int NOT NULL,
@@ -26,3 +31,46 @@ CREATE TABLE `customer_dim` (
   KEY `phone` (`phone`),
   KEY `credit_limit` (`credit_limit`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+
+#############
+# insert data
+#############
+  
+TRUNCATE TABLE retail_sales_dw.customer_dim;
+INSERT INTO `retail_sales_dw`.`customer_dim`
+(`customer_id`,
+`first_name`,
+`last_name`,
+`phone`,
+`credit_limit`)
+SELECT `customerNumber`,
+	`contactFirstName`,
+	`contactLastName`,
+	`phone`,
+	`creditLimit`
+FROM classicmodels.customers;
+SELECT * FROM retail_sales_dw.customer_dim;
+
+TRUNCATE TABLE `retail_sales_dw`.`sales_fact`;
+INSERT INTO `retail_sales_dw`.`sales_fact`
+(`sale_id`,
+`sale_date`,
+`product_id`,
+`customer_id`,
+`quantity_sold`,
+`sales_amount`)
+SELECT 
+	s.orderNumber AS sale_id,
+    s.orderDate AS sale_date,
+    sd.productCode AS product_id,
+    s.customerNumber AS customer_id,
+    sd.quantityOrdered AS quantity_sold,
+    sd.priceEach AS sales_amount
+FROM classicmodels.orders AS s
+INNER JOIN classicmodels.orderdetails AS sd ON s.orderNumber = sd.orderNumber;
+SELECT * FROM retail_sales_dw.sales_fact;
+
+SELECT * FROM retail_sales_dw.date_dim;
+
